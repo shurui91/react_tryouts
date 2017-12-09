f36229cf5551b229dc27d0931a9a2f0e96b5a224
Shurui Liu <shurui91@gmail.com>
2017-12-09 09:50:23 -0800

    config files updated

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,10 @@
-// keys.js, figure out what set of credentials to return
+module.exports = {
+	googleClientID:
+		'713354894804-ials4esbrjtik8so3h0d4uif6ste4bv8.apps.googleusercontent.com',
+	googleClientSecret: '-D4klfK6fCuqFy3pmMvnrJbs'
+};
+
+/*
 if (process.env.NODE_ENV === 'production') {
 	// we are in production mode
 	module.exports = require('./prod');
@@ -6,3 +12,4 @@ if (process.env.NODE_ENV === 'production') {
 	// we are in development mode
 	module.exports = require('./dev');
 }
+*/
