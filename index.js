dadb5e8359aadbf13728643bdecab96d1e32459c
Shurui Liu <shurui91@gmail.com>
2017-12-09 22:43:20 -0800

    added passport verification, but have bugs

--- index.js
+++ index.js
@@ -18,5 +18,15 @@ passport.use(
 	)
 );
 
+/* the first parameter 'google' is an internal way to tell
+passport to use GoogleStrategy to verify the user */
+// scope tells google to give the profile and email info
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
